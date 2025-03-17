948f0c3d774b69f20cca2ec2616600c2867c1dca
Fazdagam628 <fauzanmasud628@gmail.com>
2025-03-17 12:52:48 +0700

    tes

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -22,18 +22,22 @@ class CategorySeeder extends Seeder
         Category::create([
             'name' => 'Web Design',
             'slug' => 'web-design',
+            'color' => 'bg-yellow-500 hover:bg-yellow-700'
         ]);
         Category::create([
             'name' => 'UI UX',
             'slug' => 'ui-ux',
+            'color' => 'bg-red-500 hover:bg-red-700'
         ]);
         Category::create([
             'name' => 'Machine Learning',
             'slug' => 'machine-learning',
+            'color' => 'bg-blue-500 hover:bg-blue-700'
         ]);
         Category::create([
             'name' => 'Data Analyst',
             'slug' => 'data-analyst',
+            'color' => 'bg-green-500 hover:bg-green-700'
         ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,41 +1,58 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    @foreach ($posts as $post)
-        <article class="py-8 max-w-screen-md rounded-lg border-b border-gray-300 shadow-2xl drop-shadow-xl">
-            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-            </a>
-            <div class="">
-                By
-                <a href="/authors/{{ $post->author->username }}"
-                    class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a>
-                in
-                <a href="/categories/{{ $post->category->slug }}"
-                    class="hover:underline text-base text-gray-500">{{ $post->category->name }}</a>
-                |
-                {{ $post->created_at->diffForHumans() }}
-            </div>
-            <p class="my-4 font-light">
-                {{ Str::limit($post['body'], 100) }}
-            </p>
-            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-        </article>
-    @endforeach
-    {{-- <article>
-        <div class="container my-10 py-5 bg-gray-100">
-            <h2 class="text-center font-bold text-xl">Hello World</h2>
-            <p class="text-justify font-medium">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit
-            </p>
+
+    <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
+        <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3 ">
+            @foreach ($posts as $post)
+                <article {{-- dark:bg-gray-800 dark:border-gray-700 --}} class="p-6 bg-white rounded-lg border border-gray-200 shadow-md ">
+                    <div class="flex justify-between items-center mb-5 text-gray-500">
+                        <a href="/categories/{{ $post->category->slug }}">
+                            <span {{-- dark:bg-primary-200 dark:text-primary-800 --}}
+                                class="{{ $post->category->color }} text-white text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded ">
+
+                                {{ $post->category->name }}
+                            </span>
+                        </a>
+                        <span class="text-sm">{{ $post->created_at->diffForHumans() }}</span>
+                    </div>
+                    <a href="/posts/{{ $post->slug }}">
+                        <h2 {{-- dark:text-white --}}
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 hover:underline">
+                            {{ $post->title }}
+                        </h2>
+                    </a>
+                    <p {{-- dark:text-gray-400 --}} class="mb-5 font-light text-gray-500 ">{{ Str::limit($post['body'], 150) }}
+                    </p>
+                    <div class="flex justify-between items-center">
+                        <a href="/authors/{{ $post->author->username }}">
+                            <div class="flex items-center space-x-3">
+                                <img class="w-7 h-7 rounded-full"
+                                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                                    alt="{{ $post->author->name }}" />
+                                <span
+                                    class="font-medium text-sm
+                                    {{-- dark:text-white --}}
+                                    ">
+                                    {{ $post->author->name }}
+                                </span>
+                            </div>
+                        </a>
+                        <a href="/posts/{{ $post->slug }}"
+                            class="inline-flex items-center font-medium text-primary-600  hover:underline text-sm
+                            {{-- dark:text-primary-500 --}}
+                            ">
+                            Read more
+                            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </article>
+            @endforeach
         </div>
-    </article>
-    <article>~
-        <div class="container my-10 py-5 bg-gray-100">
-            <h2 class="text-center font-bold text-xl">Hello World</h2>
-            <p class="text-justify font-medium">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit
-            </p>
-        </div>
-    </article> --}}
+    </div>
 </x-layout>
